ecd5178307af73d60898b53490214c23089df941
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-11-08 10:55:53 +0100

    basic division

--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = props => {
-  return (
-    <h1>Hello {props.name}!</h1>
-  );
-};
-
-export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./shared/App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-ReactDOM.hydrate(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
